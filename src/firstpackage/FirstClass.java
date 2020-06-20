116ea4bb8e78e2eccf2f3e4d87011472edf06478
taner77 <tcelikim@gmail.com>
2020-06-20 14:45:53 -0400

    third uptade

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First commit from local repository");
         System.out.println("Second commit from local repository");
+        System.out.println("Third commit from local repository");
     }
 }
