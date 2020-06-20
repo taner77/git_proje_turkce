614b1d6a9f033a64ae9c17ac0c01fc009557ecfc
taner77 <tcelikim@gmail.com>
2020-06-20 15:28:55 -0400

    conflict olusturma

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First commit from local repository");
         System.out.println("Second commit from local repository");
-        System.out.println("Third commit from local repository");
-        Sysyem.out.println("First commit from  github");
+        System.out.println("Conflict olusturma");
+        System.out.println("First commit from  github");
     }
 }
