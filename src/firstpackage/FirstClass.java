fa9ea8edbef09d7a6a1da99d35ff23825d31c2bc
taner77 <58541447+taner77@users.noreply.github.com>
2020-06-20 15:16:58 -0400

    Update FirstClass.java
    First commit from github

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass {
         System.out.println("First commit from local repository");
         System.out.println("Second commit from local repository");
         System.out.println("Third commit from local repository");
+        Sysyem.out.println("First commit from  github");
     }
 }
