434441a5c6cc08895f8d17830dcc2bd0ba7030cf
taner77 <tcelikim@gmail.com>
2020-06-20 14:35:23 -0400

    second commit from local repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,5 +3,6 @@ package firstpackage;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First commit from local repository");
+        System.out.println("Second commit from local repository");
     }
 }
